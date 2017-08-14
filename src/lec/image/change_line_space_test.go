06126d7a1e29147cd5b094399f0f5648a27a5b6e
Lechuck Roh <member@lechuck2011.cafe24.com>
2017-08-14 14:59:24 +0900

    ChangeLineSpace filter test added

--- src/lec/image/change_line_space_test.go
+++ src/lec/image/change_line_space_test.go
@@ -35,5 +35,23 @@ func TestChangeLineSpace(t *testing.T) {
 		EmptyLineThreshold: 0,
 	}
 	testChangeLineSpace(t, img, opt, int(opt.HeightRatio))
+}
 
+func TestChangeLineSpaceVerticalLine(t *testing.T) {
+	img := CreateImage(100, 300, color.White)
+	FillRect(img, 50, 20, 80, 50, color.Black)
+	FillRect(img, 30, 180, 70, 200, color.Black)
+	FillRect(img, 60, 250, 100, 280, color.Black)
+	DrawLine(img, 90, 0, 90, 300, color.Black)
+
+	opt := ChangeLineSpaceOption{
+		WidthRatio:         100,
+		HeightRatio:        200,
+		LineSpaceScale:     0.1,
+		MinSpace:           1,
+		MaxRemove:          9999,
+		Threshold:          180,
+		EmptyLineThreshold: 0.02,
+	}
+	testChangeLineSpace(t, img, opt, int(opt.HeightRatio))
 }
