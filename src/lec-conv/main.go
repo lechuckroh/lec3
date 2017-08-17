2d094e19eb48edc20b80a64d3299dba6e58c8b81
LechuckRoh <member@lechuck2011.cafe24.com>
2017-08-17 21:17:32 +0900

    split main.go

--- src/lec-conv/main.go
+++ src/lec-conv/main.go
@@ -2,150 +2,35 @@ package main
 
 import (
 	"flag"
-	"image"
-	"log"
-	"path"
-	"reflect"
-	"runtime"
-	"sync"
 
-	limg "lec/image"
 	llog "lec/log"
 )
 
-//-----------------------------------------------------------------------------
-// Work
-//-----------------------------------------------------------------------------
-
-// Work contains an information of image file to process
-type Work struct {
-	dir      string
-	filename string
-	quit     bool
-}
-
-// Worker is channel of Work
-type Worker struct {
-	workChan <-chan Work
-}
-
-func collectImages(workChan chan<- Work, finChan chan<- bool, srcDir string) {
-	defer func() {
-		finChan <- true
-	}()
-
-	// List image files
-	files, err := limg.ListImages(srcDir)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	// add works
-	for _, file := range files {
-		workChan <- Work{srcDir, file.Name(), false}
-	}
-}
-
-func work(worker Worker, filters []limg.Filter, config *Config, wg *sync.WaitGroup) {
-	defer func() {
-		wg.Done()
-	}()
-
-	for {
-		work := <-worker.workChan
-		if work.quit {
-			break
-		}
-
-		log.Printf("[READ] %v\n", work.filename)
-
-		src, err := limg.LoadImage(path.Join(work.dir, work.filename))
-		if err != nil {
-			log.Printf("Error : %v : %v\n", work.filename, err)
-			continue
-		}
-
-		// run filters
-		var dest image.Image
-		for _, filter := range filters {
-			result := filter.Run(limg.NewFilterSource(src, work.filename))
-			result.Log()
-
-			resultImg := result.Img()
-			if resultImg == nil {
-				log.Printf("Filter result is nil. filter: %v\n", reflect.TypeOf(filter))
-				break
-			}
-
-			dest = resultImg
-			src = dest
-		}
-
-		// resize
-		dest = limg.ResizeImage(dest, config.width, config.height)
-
-		// save dest Img
-		// ---------------
-		filename := config.FormatDestFilename(work.filename)
-		err = limg.SaveJpeg(dest, config.dest.dir, filename, 80)
-		if err != nil {
-			log.Printf("Error : %v : %v\n", filename, err)
-			continue
-		}
-	}
-}
-
-func main() {
-	llog.SetLogPattern(llog.TimeOnly)
-
+func getConfig() *Config {
 	cfgFilename := flag.String("cfg", "", "configuration filename")
 	srcDir := flag.String("src", "", "source directory")
 	destDir := flag.String("dest", "", "dest directory")
 	flag.Parse()
 
-	// Print usage
-	if *cfgFilename == "" || (flag.NFlag() == 1 && flag.Arg(1) == "help") {
-		flag.Usage()
-		return
+	if *cfgFilename == "" {
+		return nil
 	}
 
 	// create Config
 	config := NewConfig(*cfgFilename, *srcDir, *destDir)
+	return config
+}
+
+func main() {
+	llog.SetLogPattern(llog.TimeOnly)
+
+	config := getConfig()
+	if config == nil || (flag.NFlag() == 1 && flag.Arg(1) == "help") {
+		flag.Usage()
+		return
+	}
+
 	config.Print()
 
-	// set maxProcess
-	runtime.GOMAXPROCS(config.maxProcess)
-
-	// Create channels
-	workChan := make(chan Work, 100)
-	finChan := make(chan bool)
-
-	// WaitGroup
-	wg := sync.WaitGroup{}
-
-	// start collector
-	go collectImages(workChan, finChan, config.src.dir)
-
-	var filters []limg.Filter
-	for _, filterOption := range config.filterOptions {
-		filters = append(filters, filterOption.filter)
-	}
-
-	// start workers
-	for i := 0; i < config.maxProcess; i++ {
-		worker := Worker{workChan}
-		wg.Add(1)
-		go work(worker, filters, config, &wg)
-	}
-
-	// wait for collector finish
-	<-finChan
-
-	// finish workers
-	for i := 0; i < config.maxProcess; i++ {
-		workChan <- Work{"", "", true}
-	}
-
-	wg.Wait()
+	startWorks(config)
 }
---
+++ src/lec-ip/work.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"image"
+	"log"
+	"os"
+	"path"
+	"reflect"
+	"runtime"
+	"sync"
+	"time"
+
+	limg "lec/image"
+)
+
+// Work represents a job to do
+type Work struct {
+	dir      string
+	filename string
+	quit     bool
+}
+
+// Worker is a worker to process images.
+type Worker struct {
+	workChan <-chan Work
+}
+
+func collectImages(workChan chan<- Work, finChan chan<- bool, srcDir string, watch bool, watchDelay int) {
+	defer func() {
+		finChan <- true
+	}()
+
+	lastCheckTime := time.Unix(0, 0)
+	var files []os.FileInfo
+	var err error
+
+	for {
+		// List modified image files
+		files, lastCheckTime, err = limg.ListModifiedImages(srcDir, watchDelay, lastCheckTime)
+		if err != nil {
+			log.Println(err)
+			break
+		}
+
+		// add works
+		for _, file := range files {
+			workChan <- Work{srcDir, file.Name(), false}
+		}
+
+		if watch {
+			// sleep for a while
+			time.Sleep(time.Duration(5) * time.Second)
+		} else {
+			break
+		}
+	}
+}
+
+func work(worker Worker, filters []limg.Filter, destDir string, wg *sync.WaitGroup) {
+	defer func() {
+		wg.Done()
+	}()
+
+	for {
+		work := <-worker.workChan
+		if work.quit {
+			break
+		}
+
+		log.Printf("[R] %v\n", work.filename)
+
+		src, err := limg.LoadImage(path.Join(work.dir, work.filename))
+		if err != nil {
+			log.Printf("Error : %v : %v\n", work.filename, err)
+			continue
+		}
+
+		// run filters
+		var dest image.Image
+		for _, filter := range filters {
+			result := filter.Run(limg.NewFilterSource(src, work.filename))
+			result.Log()
+
+			resultImg := result.Img()
+			if resultImg == nil {
+				log.Printf("Filter result is nil. filter: %v\n", reflect.TypeOf(filter))
+				break
+			}
+
+			dest = resultImg
+			src = dest
+		}
+
+		// save dest Img
+		err = limg.SaveJpeg(dest, destDir, work.filename, 80)
+		if err != nil {
+			log.Printf("Error : %v : %v\n", work.filename, err)
+			continue
+		}
+	}
+}
+
+func startWorks(config *Config) {
+	// set maxProcess
+	runtime.GOMAXPROCS(config.maxProcess)
+
+	// Create channels
+	workChan := make(chan Work, 100)
+	finChan := make(chan bool)
+
+	// WaitGroup
+	wg := sync.WaitGroup{}
+
+	// start collector
+	go collectImages(workChan, finChan, config.src.dir, config.watch, config.watchDelay)
+
+	var filters []limg.Filter
+	for _, filterOption := range config.filterOptions {
+		filters = append(filters, filterOption.filter)
+	}
+
+	// start workers
+	for i := 0; i < config.maxProcess; i++ {
+		worker := Worker{workChan}
+		wg.Add(1)
+		go work(worker, filters, config.dest.dir, &wg)
+	}
+
+	// wait for collector finish
+	<-finChan
+
+	// finish workers
+	for i := 0; i < config.maxProcess; i++ {
+		workChan <- Work{"", "", true}
+	}
+
+	wg.Wait()
+}
